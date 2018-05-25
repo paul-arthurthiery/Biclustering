6f5be6802d6227aef42011ebf9a604fb7adedb29
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-05-25 17:45:18 +0200

    Initial Flag class and txt data file

---
+++ Flag.py
@@ -0,0 +1,33 @@
+class Flag:
+
+    def _init_(self, name, landmass, zone, area, population, language, religion, bars, stripes, colours, red, green, blue, gold, white, black, orange, mainhue, circles, crosses, saltires, quarters, sunstars, crescent, triangle, icon, animate, text, topleft, botright):
+        self.name = name
+        self.landmass = landmass
+        self.zone = zone
+        self.area = area
+        self.population = population
+        self.language = language
+        self.religion = religion
+        self.bars = bars
+        self.stripes = stripes
+        self.colours = colours
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.gold = gold
+        self.white = white
+        self.black = black
+        self.orange = orange
+        self.mainhue = mainhue
+        self.circles = circles
+        self.crosses = crosses
+        self.saltires = saltires
+        self.quarters = quarters
+        self.sunstars = sunstars
+        self.crescent = crescent
+        self.triangle = triangle
+        self.icon = icon
+        self.animate = animate
+        self.text = text
+        self.topleft = topleft
+        self.botright = botright
