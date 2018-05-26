a0dff47ad0d5481cf48b77ea8a31fd7cdaa59a90
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-05-26 17:13:31 +0200

    New comparison methods, everything switched to arrays, etc

--- src/main/java/ClusterTool.java
+++ src/main/java/ClusterTool.java
@@ -7,66 +7,40 @@ import java.util.List;
 import static java.lang.Math.abs;
 
 public class ClusterTool {
-    private List<List<Flag>> listOfClusters, oneCluster;
     private Flag[] listOfFlags;
 
     public ClusterTool(Flag[] listOfFlags) {
         this.listOfFlags = listOfFlags;
-        this.listOfClusters = new ArrayList<List<Flag>>();
-        this.oneCluster = new ArrayList<List<Flag>>();
-        this.oneCluster.add(new ArrayList<Flag>());
-        for(int i=0; i<6; i++){
-            ArrayList<Flag> cluster = new ArrayList<Flag>();
-            listOfClusters.add(cluster);
-        }
     }
 
-    //generate a first list of clusters by grouping all flags from similar continents
-    public List<List<Flag>> getLandmassClusters() {
-        for(int i=0; i<this.listOfFlags.length; i++){
-            this.listOfClusters.get(this.listOfFlags[i].landmass-1).add(this.listOfFlags[i]);
-        }
-        return this.listOfClusters;
-    }
 
-    // groups all the flags into one cluster
-    public List<List<Flag>> getOneCluster()
+    public List<List<Flag>> biCluster()
     {
-        for(Flag flag : this.listOfFlags) {
-            this.oneCluster.get(0).add(flag);
-        }
-        return this.oneCluster;
-    }
-
-    public List<List<Flag>> biCluster(List<List<Flag>> singleClusteredList)
-    {
+        Flag[] singleClusteredList = this.listOfFlags;
         //TODO DONE : List<List<List<Integer>>> distances = new ArrayList de matrice pour chaque cluster;
         //TODO DONE : fill matrix with distance between each element in cluster (start by doing the absolute value of the difference between each variable of each Flag in the cluster)
 
         long millisStart = Calendar.getInstance().getTimeInMillis();
         // loops through all the clusters and all the flags intra-cluster to compare flags one by one,
         // and store an array with the distances between each
-        List<int[][]> distances = new ArrayList<int[][]>();
-        for(List<Flag> cluster : singleClusteredList)
+        int[][] distances = new int[singleClusteredList.length][singleClusteredList.length];
+        for(int i=0; i<singleClusteredList.length;i++)
         {
-            int[][] clusterMatrix = new int[cluster.size()][cluster.size()];
-            for(int i=0; i<cluster.size();i++)
+            Flag flagOne = singleClusteredList[i];
+            distances[i][i] = 0;
+            for(int j=i+1; j<singleClusteredList.length;j++)
             {
-                Flag flagOne = cluster.get(i);
-                clusterMatrix[i][i] = 0;
-                for(int j=i+1; j<cluster.size();j++)
-                {
-                    Flag flagTwo = cluster.get(j);
-                    clusterMatrix[i][j] = clusterMatrix[j][i] = compareTwoFlags(flagOne, flagTwo);
-                }
+                Flag flagTwo = singleClusteredList[j];
+                distances[i][j] = distances[j][i] = compareTwoFlags(flagOne, flagTwo);
             }
-            distances.add(clusterMatrix);
         }
+
         long millisEnd = Calendar.getInstance().getTimeInMillis();
         System.out.println("Distance Done Time : "+(millisEnd-millisStart));
 
         //TODO DONE : create ArrayList of ArrayLists of Flag
-        List<List<Flag>> reClusteredFlags = new ArrayList<List<Flag>>();
+        List<List<Flag>> biClusteredFlags = new ArrayList<List<Flag>>();
+
 
         //TODO : for each group of flags in cluster with a distance below the threshold, create a new cluster
         //TODO : add all these flags to the cluster
@@ -76,57 +50,66 @@ public class ClusterTool {
         // Nombre de paramètres minimum pour former un cluster ?
         // Regrouper en catégorie "nbr de critères similaires" ?
         // Créer une note de "matching" et regrouper ensuite ?
-        
 
 
-
-        return reClusteredFlags;
+        return biClusteredFlags;
     }
 
     // method to compare attributes of two flags, returns the difference in an array
     private int compareTwoFlags(Flag flagOne, Flag flagTwo)
     {
         int distance = 0;
-        distance += (abs(stringCompare(flagOne.name,flagTwo.name)));
-        distance += (abs(flagOne.landmass-flagTwo.landmass));
-        distance += (abs(flagOne.zone-flagTwo.zone));
-        distance += (abs(flagOne.area-flagTwo.area));
-        distance += (abs(flagOne.population-flagTwo.population));
-        distance += (abs(flagOne.language-flagTwo.language));
-        distance += (abs(flagOne.religion-flagTwo.religion));
-        distance += (abs(flagOne.bars-flagTwo.bars));
-        distance += (abs(flagOne.stripes-flagTwo.stripes));
-        distance += (abs(flagOne.colours-flagTwo.colours));
-        distance += (abs(flagOne.red-flagTwo.red));
-        distance += (abs(flagOne.green-flagTwo.green));
-        distance += (abs(flagOne.blue-flagTwo.blue));
-        distance += (abs(flagOne.gold-flagTwo.gold));
-        distance += (abs(flagOne.white-flagTwo.white));
-        distance += (abs(flagOne.black-flagTwo.black));
-        distance += (abs(flagOne.orange-flagTwo.orange));
-        distance += (abs(stringCompare(flagOne.mainhue,flagTwo.mainhue)));
-        distance += (abs(flagOne.circles-flagTwo.circles));
-        distance += (abs(flagOne.crosses-flagTwo.crosses));
-        distance += (abs(flagOne.saltires-flagTwo.saltires));
-        distance += (abs(flagOne.quarters-flagTwo.quarters));
-        distance += (abs(flagOne.sunstars-flagTwo.sunstars));
-        distance += (abs(flagOne.crescent-flagTwo.crescent));
-        distance += (abs(flagOne.triangle-flagTwo.triangle));
-        distance += (abs(flagOne.icon-flagTwo.icon));
-        distance += (abs(flagOne.animate-flagTwo.animate));
-        distance += (abs(flagOne.text-flagTwo.text));
-        distance += (abs(stringCompare(flagOne.topleft,flagTwo.topleft)));
-        distance += (abs(stringCompare(flagOne.botright,flagTwo.botright)));
+        distance += stringCompare(flagOne.name,flagTwo.name);
+        distance += nonSubtractionCompare(flagOne.landmass,flagTwo.landmass);
+        distance += nonSubtractionCompare(flagOne.zone,flagTwo.zone);
+        distance += subtractionCompare(flagOne.area, flagTwo.area);
+        distance += subtractionCompare(flagOne.population, flagTwo.population);
+        distance += nonSubtractionCompare(flagOne.language,flagTwo.language);
+        distance += nonSubtractionCompare(flagOne.religion,flagTwo.religion);
+        distance += subtractionCompare(flagOne.bars,flagTwo.bars);
+        distance += subtractionCompare(flagOne.stripes, flagTwo.stripes);
+        distance += subtractionCompare(flagOne.colours, flagTwo.colours);
+        distance += nonSubtractionCompare(flagOne.red, flagTwo.red);
+        distance += nonSubtractionCompare(flagOne.green, flagTwo.green);
+        distance += nonSubtractionCompare(flagOne.blue, flagTwo.blue);
+        distance += nonSubtractionCompare(flagOne.gold, flagTwo.gold);
+        distance += nonSubtractionCompare(flagOne.white, flagTwo.white);
+        distance += nonSubtractionCompare(flagOne.black, flagTwo.black);
+        distance += nonSubtractionCompare(flagOne.orange, flagTwo.orange);
+        distance += stringCompare(flagOne.mainhue,flagTwo.mainhue);
+        distance += subtractionCompare(flagOne.circles, flagTwo.circles);
+        distance += subtractionCompare(flagOne.crosses, flagTwo.crosses);
+        distance += subtractionCompare(flagOne.saltires, flagTwo.saltires);
+        distance += subtractionCompare(flagOne.quarters, flagTwo.quarters);
+        distance += subtractionCompare(flagOne.sunstars, flagTwo.sunstars);
+        distance += nonSubtractionCompare(flagOne.crescent, flagTwo.crescent);
+        distance += nonSubtractionCompare(flagOne.triangle, flagTwo.triangle);
+        distance += nonSubtractionCompare(flagOne.icon, flagTwo.icon);
+        distance += nonSubtractionCompare(flagOne.animate, flagTwo.animate);
+        distance += nonSubtractionCompare(flagOne.text, flagTwo.text);
+        distance += stringCompare(flagOne.topleft,flagTwo.topleft);
+        distance += stringCompare(flagOne.botright,flagTwo.botright);
         return distance;
     }
 
 
-    // method to compare two String, return 0 if identic, 1 otherwise
+    // method to compare two String, return 0 if identical, 1 otherwise
     private int stringCompare(String a, String b)
     {
-        return a.matches(b) ? 0 : 1;
+        return a.matches(b) ? 0 : 100;
     }
 
+    //method to compare 2 ints, when subtraction doesn't represent closeness. Same output as above
+    private int nonSubtractionCompare(int a, int b){
+        return a == b ? 0: 100;
+    }
+
+    //method to compare 2 ints, when subtraction represents closeness.
+    private int subtractionCompare(int a, int b){
+        if(a == b) return 0;
+        int max = a>b ? a : b;
+        return (abs(a-b)/max)*100;
+    }
 
 
 }
--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -9,11 +9,8 @@ public class Testing
         FileProcessor processor = new FileProcessor();
         Flag[] flags = processor.processFile("flag.txt");
         ClusterTool clusterer = new ClusterTool(flags);
-        List<List<Flag>> result = clusterer.getLandmassClusters();
-        List<List<Flag>> oneCluster = clusterer.getOneCluster();
-        clusterer.biCluster(result);
         System.out.println("###");
-        clusterer.biCluster(oneCluster);
+        clusterer.biCluster();
 
 
     }
