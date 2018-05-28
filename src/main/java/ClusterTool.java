6586c864b5cd9763301abf25033c47511c713ad4
Alexvanwee <alexandre.vanwesel@gmail.com>
2018-05-28 05:45:47 +0100

    New initial clusters algorithm + some cleaning

--- src/main/java/ClusterTool.java
+++ src/main/java/ClusterTool.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 import static java.lang.Math.abs;
 
-public class ClusterTool {
+public class ClusterTool
+{
     private Flag[] listOfFlags;
     private int numberOfClusters;
 
@@ -17,22 +18,19 @@ public class ClusterTool {
         this.numberOfClusters = numberOfClusters;
     }
 
-
+    // main method to pilot all the biclustering process
     public List<List<Flag>> biCluster()
     {
         Flag[] singleClusteredList = this.listOfFlags.clone();
-        //TODO DONE : List<List<List<Integer>>> distances = new ArrayList de matrice pour chaque cluster;
-        //TODO DONE : fill matrix with distance between each element in cluster (start by doing the absolute value of the difference between each variable of each Flag in the cluster)
 
         long millisStart = Calendar.getInstance().getTimeInMillis();
         // loops through all the clusters and all the flags intra-cluster to compare flags one by one,
         // and store an array with the distances between each
 
         int numberOfFlags = this.listOfFlags.length;
-        int[] arrayAllPairDistances = new int[(numberOfFlags*(numberOfFlags-1))/2];
-        int[][] flagPairs = new int[arrayAllPairDistances.length][2];
-        //TODO DONE: add average distance with every other flag for each flag
-        int[][] distances = new int[singleClusteredList.length][singleClusteredList.length];
+        int[] arrayAllPairDistances = new int[(numberOfFlags*(numberOfFlags-1))/2]; //one dimension array with all the distances of the possible pairs of flags
+        int[][] flagPairs = new int[arrayAllPairDistances.length][2]; //all the possible pairs of flags
+        int[][] distances = new int[singleClusteredList.length][singleClusteredList.length];//all pairs distances in 2D
         int[] averageDistancePerFlag = new int[listOfFlags.length];
         int counter = 0;
         for(int i=0; i<singleClusteredList.length;i++)
@@ -55,7 +53,6 @@ public class ClusterTool {
         long millisEnd = Calendar.getInstance().getTimeInMillis();
         System.out.println("Distance Done Time : "+(millisEnd-millisStart));
 
-        //TODO DONE: remove 20% of flags with the lowest/highest average distance compared to median value (removes the aberrant values)
         millisStart = Calendar.getInstance().getTimeInMillis();
 
         ArrayIndexComparator comparator = new ArrayIndexComparator(arrayAllPairDistances);
@@ -64,9 +61,9 @@ public class ClusterTool {
 
         int position = (indexes.length/2)-1; // position of medianIndex in indexes array
         int medianIndex = indexes[position]; // value of the index of the median
-        int medianDistance = arrayAllPairDistances[medianIndex];
-        // percentage to keep, centered on the median (half above and under)
-        float percentageToKeep = 80;
+        float percentageToKeep = 80; // percentage to keep, centered on the median (half above and under)
+        //Percentage : Max clusters / 40%:8 / 50%:10 / 60%:20 / 70%:22 / 80%:32
+        // 60% and 15 clusters is nice, more tests to do
         int bottomIndex = (int)((position)-((percentageToKeep/200.0)*indexes.length));
         int topIndex = (int)((position)+((percentageToKeep/200.0)*indexes.length));
         Integer[] indexesOfFlagsToKeep = Arrays.copyOfRange(indexes, bottomIndex,topIndex+1);
@@ -74,16 +71,16 @@ public class ClusterTool {
         millisEnd = Calendar.getInstance().getTimeInMillis();
         System.out.println("Sorting and Median Done Time : " +(millisEnd-millisStart));
 
-        millisStart = Calendar.getInstance().getTimeInMillis();
+        //millisStart = Calendar.getInstance().getTimeInMillis();
 
-        //#### INFO : to know what pair of flags is at an index, just do :
-        //########### --> flagPairs[index]  // returns int[flagOne,flagTwo]
-        //TODO DONE : create ArrayList of ArrayLists of Flag
+        /*#### INFO : to know what pair of flags is at an index, just do :
+        ########### --> flagPairs[index]  // returns int[flagOne,flagTwo] */
+
         List<List<Flag>> biClusteredFlags = new ArrayList<List<Flag>>();
-        //TODO DONE: next find the n best distances to create the clusters
-        //TODO DONE: create n ArrayLists in biClusteredFlags. Add to each one of the n remaining flags with the largest pair distance but the smallest average distance
         Stack remainingFlagsStack = new Stack<Flag>();
         remainingFlagsStack.addAll(Arrays.asList(listOfFlags));
+
+        /*
         for(int k=indexesOfFlagsToKeep.length-1; k>indexesOfFlagsToKeep.length-(1+numberOfClusters); k--){
             ArrayList<Flag> cluster = new ArrayList<Flag>();
             int[] largeDistancePair = flagPairs[indexesOfFlagsToKeep[k]];
@@ -95,11 +92,18 @@ public class ClusterTool {
 
         millisEnd = Calendar.getInstance().getTimeInMillis();
         System.out.println("First Element Clusters Done Time : " +(millisEnd-millisStart));
+        */
 
+        //########################### NEW ALGO #############################################
+        millisStart = Calendar.getInstance().getTimeInMillis();
+        Object[] firstClusteringResult = findFirstClustersFlags(remainingFlagsStack,indexesOfFlagsToKeep,flagPairs,arrayAllPairDistances);
+        biClusteredFlags = (List<List<Flag>>) firstClusteringResult[0];
+        remainingFlagsStack = (Stack) firstClusteringResult[1];
+        millisEnd = Calendar.getInstance().getTimeInMillis();
+        System.out.println("Custom Spicy First Clusters Done Time : " +(millisEnd-millisStart));
+        //###################################################################################
 
-        //TODO DONE : create a method which takes in parameter the array of distances, the current list of clusters, and a stack of remaining Flags
         //for each cluster, it will add the closest flag to the first flag in the array
-
         millisStart = Calendar.getInstance().getTimeInMillis();
         biClusteredFlags = biClusterFromFirstElements(biClusteredFlags, distances, remainingFlagsStack);
         millisEnd = Calendar.getInstance().getTimeInMillis();
@@ -110,6 +114,7 @@ public class ClusterTool {
         return biClusteredFlags;
     }
 
+    // method to display the flags of the result of the bicluster
     public void displayClustersFlags(List<List<Flag>> clusters)
     {
         int counter = 1;
@@ -134,14 +139,14 @@ public class ClusterTool {
             counter++;
         }
     }
-
+    // resizes an ImageIcon
     private ImageIcon resizeIcon(ImageIcon icon, int width, int height)
     {
         Image img = icon.getImage();
         Image newimg = img.getScaledInstance(width, height,  java.awt.Image.SCALE_SMOOTH);
         return new ImageIcon(newimg);
     }
-
+    // shows the frame with the flags
     private void showFrame(JPanel pane, String title)
     {
         JFrame mainframe = new JFrame(title);
@@ -205,7 +210,8 @@ public class ClusterTool {
         int max = a>b ? a : b;
         return (abs(a-b)/max)*100;
     }
-
+    /* custom comparator to sort an array of indexes (generated from an initial array)
+     instead of sorting the initial array */
     public class ArrayIndexComparator implements Comparator<Integer> {
         private final int[] array;
 
@@ -229,9 +235,9 @@ public class ClusterTool {
             return array[index1] <= array[index2] ? (array[index1] < array[index2] ? -1 : 0) : 1;
         }
     }
-
+    // dispatches the remaining flags into the clusters they are closest to
     public List<List<Flag>> biClusterFromFirstElements(List<List<Flag>> biclusters, int[][] distances, Stack<Flag> remainingFlagsStack){
-        List<Flag> arrayListOfFlags = Arrays.asList(listOfFlags);
+        List<Flag> arrayListOfFlags = Arrays.asList(this.listOfFlags);
         while(!remainingFlagsStack.empty()){
             Flag currentFlag = remainingFlagsStack.pop();
             int clusterToAddFlagIndex = numberOfClusters-1;
@@ -248,12 +254,207 @@ public class ClusterTool {
         }
         return biclusters;
     }
-
+    //prints the result of the biclustering
     public void printBicluster(List<List<Flag>> biClusteredFlags){
         for(int i=0; i<biClusteredFlags.size();i++){
             System.out.println(biClusteredFlags.get(i));
         }
     }
+    // method to call to generate the clusters with a first flag inside
+    private Object[] findFirstClustersFlags(Stack remainingFlagsStack, Integer[] indexesOfFlagsToKeep, int[][] flagPairs, int[] arrayAllPairDistances)
+    {
+        Object[] toReturn = new Object[2];;
+        if(this.numberOfClusters==2)
+        {
+            toReturn = findFirstClustersForTwo(indexesOfFlagsToKeep, flagPairs, remainingFlagsStack, toReturn);
+        }
+        else
+        {
+            toReturn = findFirstClustersForMore(remainingFlagsStack, flagPairs, indexesOfFlagsToKeep, arrayAllPairDistances, toReturn);
+        }
+        return toReturn;
+    }
+    // called from upper method to do what said above, called if the number of clusters requested is > 2
+    private Object[] findFirstClustersForMore(Stack remainingFlagsStack, int[][] flagPairs, Integer[] indexesOfFlagsToKeep, int[] arrayAllPairDistances, Object[] toReturn)
+    {
+        int[][] matrixOfFlagsToKeep = new int[this.listOfFlags.length][this.listOfFlags.length];
+        for(int index : indexesOfFlagsToKeep)
+        {
+            int[] pair = flagPairs[index];
+            matrixOfFlagsToKeep[pair[0]][pair[1]] = 1;
+        }
 
+        List<List<Flag>> biClusteredFlags = new ArrayList<List<Flag>>();
+        ArrayList<int[]> pairs = new ArrayList<int[]>();
+        int horizontalMoving = this.numberOfClusters-2;
+        horizontalMoving = this.numberOfClusters==3 ? 2 : (this.numberOfClusters==2 ? 1 : horizontalMoving);
+        int verticalMoving = 2;
+        verticalMoving = this.numberOfClusters==2 || this.numberOfClusters==3 ? 1 : verticalMoving;
+        int vertical = 0;
+        ArrayList<ArrayList<Integer>> possibilities = new ArrayList<ArrayList<Integer>>();
+        while(vertical<matrixOfFlagsToKeep.length)
+        {
+            for(int horizontal=0; horizontal<matrixOfFlagsToKeep.length-horizontalMoving;horizontal++)
+            {
+                ArrayList<Integer> verticalFlags = new ArrayList<Integer>();
+                ArrayList<Integer> horizontalFlags = new ArrayList<Integer>();
+                ArrayList<int[]> thepairs = new ArrayList<int[]>();
+                int cancelToken = 0;
+                if(horizontal<2&&vertical<2){}
+                else if(horizontal!=vertical)
+                {
+                    for (int i = 0; i < horizontalMoving; i++)
+                    {
+                        int breakToken = 0;
+                        int horizontalIndex = horizontal + i;
+                        for (int j = 0; j < verticalMoving; j++)
+                        {
+                            int verticalIndex = vertical + j;
+                            int value = matrixOfFlagsToKeep[verticalIndex][horizontalIndex];
+                            if (value == 0)
+                            {
+                                breakToken = 1;
+                                verticalFlags.clear();
+                                horizontalFlags.clear();
+                                thepairs.clear();
+                                break;
+                            }
+                            if (!verticalFlags.contains(verticalIndex))
+                            {
+                                verticalFlags.add(verticalIndex);
+                            }
+                            if(!horizontalFlags.contains(horizontalIndex))
+                            {
+                                horizontalFlags.add(horizontalIndex);
+                            }
+                            thepairs.add(new int[]{horizontalIndex, verticalIndex});
+                        }
+                        if (breakToken == 1)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if((verticalFlags.size()+horizontalFlags.size())==this.numberOfClusters)
+                {
+                    if(verticalFlags.size()==2)
+                    {
+                        if (matrixOfFlagsToKeep[verticalFlags.get(0)][verticalFlags.get(1)] == 0)
+                        {
+                            verticalFlags.clear();
+                            horizontalFlags.clear();
+                            thepairs.clear();
+                            cancelToken = 1;
+                        }
+                    }
+                    else
+                    {
+                        for(int a=0; a<horizontalFlags.size()-1;a++)
+                        {
+                            int count = 0;
+                            int valueOne = horizontalFlags.get(a);
+                            for(int b=a+1; b<horizontalFlags.size();b++)
+                            {
+                                int valueTwo = horizontalFlags.get(b);
+                                count += matrixOfFlagsToKeep[valueOne][valueTwo];
+                            }
+                            if(count==0)
+                            {
+                                cancelToken = 1;
+                            }
+                        }
+                    }
+                    if(cancelToken==0)
+                    {
+                        horizontalFlags.addAll(verticalFlags);
+                        ArrayList<Integer> flags = new ArrayList<Integer>(horizontalFlags);
+                        possibilities.add(flags);
+                        //####################################
+                        /*
+                        for(int flagNum : flags)
+                        {
+                            Flag flag = this.listOfFlags[flagNum];
+                            ArrayList<Flag> cluster = new ArrayList<Flag>();
+                            cluster.add(flag);
+                            remainingFlagsStack.remove(flag);
+                            biClusteredFlags.add(cluster);
+                        }
+                        pairs = (ArrayList<int[]>) thepairs.clone();
+                        vertical = matrixOfFlagsToKeep.length;
+                        */
+                        //################################
+                        break;
+
+                    }
+                }
+            }
+            vertical++;
+        }
+        //############################################
+        /*
+        for(int[] a : pairs)
+        {
+            System.out.print(Arrays.toString(a));
+        }
+        System.out.println();
+        */
+        //#############################################
+
+        //#############################################
+        ///*
+        int[] clustersDistances = new int[possibilities.size()];
+        for(int i=0; i<possibilities.size();i++)
+        {
+            ArrayList<Integer> cluster = possibilities.get(i);
+            int distance = 0;
+            for(int flag : cluster)
+            {
+                distance += arrayAllPairDistances[flag];
+            }
+            clustersDistances[i] = distance;
+        }
+        ArrayIndexComparator comparator = new ArrayIndexComparator(clustersDistances);
+        Integer[] indexes = comparator.createIndexArray();
+        Arrays.sort(indexes, comparator);
+        int bestCluster = indexes[indexes.length-1];
+        System.out.println("Found Possibilities : "+possibilities.size()+" , Selected One : "+bestCluster);
+        for(int flagNum : possibilities.get(bestCluster))
+        {
+            System.out.print(flagNum+" / ");
+            Flag flag = this.listOfFlags[flagNum];
+            ArrayList<Flag> cluster = new ArrayList<Flag>();
+            cluster.add(flag);
+            remainingFlagsStack.remove(flag);
+            biClusteredFlags.add(cluster);
+        }
+        System.out.println();
+        //*/
+        //####################################
+
+        toReturn[0] = biClusteredFlags;
+        toReturn[1] = remainingFlagsStack;
+        return toReturn;
+    }
+    // same as above but when number of clusters requested is 2
+    private Object[] findFirstClustersForTwo(Integer[] indexesOfFlagsToKeep, int[][] flagPairs, Stack remainingFlagsStack, Object[] toReturn)
+    {
+        List<List<Flag>> biClusteredFlags = new ArrayList<List<Flag>>();
+        int bestPairIndex = indexesOfFlagsToKeep[indexesOfFlagsToKeep.length-1];
+        int[] bestPair = flagPairs[bestPairIndex];
+        Flag flagOne = this.listOfFlags[bestPair[0]];
+        Flag flagTwo = this.listOfFlags[bestPair[1]];
+        List<Flag> clusterOne = new ArrayList<Flag>();
+        clusterOne.add(flagOne);
+        biClusteredFlags.add(clusterOne);
+        remainingFlagsStack.remove(flagOne);
+        List<Flag> clusterTwo = new ArrayList<Flag>();
+        clusterTwo.add(flagTwo);
+        remainingFlagsStack.remove(flagTwo);
+        biClusteredFlags.add(clusterTwo);
+        toReturn[0] = biClusteredFlags;
+        toReturn[1] = remainingFlagsStack;
+        System.out.println(Arrays.toString(bestPair));
+        return toReturn;
+    }
 
 }
--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -12,10 +12,9 @@ public class Testing
 
         FileProcessor processor = new FileProcessor();
         Flag[] flags = processor.processFile("flag.txt");
-        ClusterTool clusterer = new ClusterTool(flags, 6);
+        ClusterTool clusterer = new ClusterTool(flags, 15);
         List<List<Flag>> result = clusterer.biCluster();
         clusterer.displayClustersFlags(result);
-
 
     }
 
